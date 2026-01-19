5c37ea9bc90266be63db94b7fecb6c3dabca2c4c
Vishwajeet <vishwajeetadkine705@gmail.com>
2026-01-19 17:50:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
   server: {
     port: 3000,
     host: '0.0.0.0',
